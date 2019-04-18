c6b461d8056ac0a9a6742bcc96674a9981bd0ea3
Dylan Laufenberg <edev455@gmail.com>
2019-04-17 23:49:26 -0700

    Create four conversion methods and write test functions.

---
+++ TermProject/TestConvert/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WisconsinSetup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            convertRange(DataFactory.OrderedConvert, 0, 32);
+
+            long start = 0;
+            long end = 47995000;
+            //testRange(DataFactory.KavinConvert, start, end);
+            //testRange(DataFactory.Convert, start, end);
+            // testRange(DataFactory.PaperConvert, start, end, true);
+            // testRange(DataFactory.OrderedConvert, start, end); // Good! No duplicates found.
+
+            Console.ReadLine();
+        }
+
+        static void convertRange(Func<long, string> converter, long start, long end)
+        {
+            for (long i = start; i <= end; i++)
+            {
+                convert(converter, i);
+            }
+        }
+
+        static void convert(Func<long, string> converter, long number)
+        {
+            Console.WriteLine($"{number}\t->  {converter(number)}");
+        }
+
+        static Boolean testRange(Func<long, string> converter, long start, long end, Boolean continueOnDuplicate=false)
+        {
+            Dictionary<string, long> table = new Dictionary<string, long>();
+            long duplicatesFound = 0;
+
+            for (long i = start; i <= end; i++)
+            {
+                string output = converter(i);
+                if (table.ContainsKey(output))
+                {
+                    Console.WriteLine($"Duplicate found! Both {table[output]} and {i} map to {output}");
+                    duplicatesFound++;
+                    if (continueOnDuplicate == false)
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    table.Add(output, i);
+                }
+            }
+            if (duplicatesFound == 0)
+            {
+                Console.WriteLine("No duplicates found.");
+            }
+            else
+            {
+                Console.WriteLine($"Found {duplicatesFound} duplicates.");
+            }
+            return true;
+        }
+    }
+}
---
+++ TermProject/WisconsinSetup/DataFactory.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.TextFormatting;
+
+namespace WisconsinSetup
+{
+    public class DataFactory
+    {
+
+        public static string PaperConvert(long unique)
+        {
+            char[] tmp = new char[7];
+            char[] result = new char[7];
+            long i, j, remainder, count;
+
+            /* Set the result string to 'AAAAAA' initially. */
+            for (i = 0; i < 7; i++)
+            {
+                result[i] = 'A';
+            }
+
+            /* Convert unique values from right to left into an alphabetic string in tmp */
+            /* temp digits are right-justified in tmp */
+            i = 6;
+            count = 0;
+            while (unique > 0)
+            {
+                remainder = unique % 26;
+                tmp[i] = (char) ('A' + remainder);
+                unique /= 26;
+                i--;
+                count++;
+            }
+
+            /* FIXED: increment i beforehand. */
+            i++;
+
+            /* Finally, move tmp into result, left justifying it. */
+            /* FIXED: j < count. */
+            for (j = 0; j < count; j++, i++)
+            {
+                result[j] = tmp[i];
+            }
+            return new string(result);
+        }
+
+        public static string KavinConvert(long unique)
+        {
+            char[] tmp = new char[7];
+            char[] result = new char[7];
+            long i, j, remainder, count;
+
+            /* Set the result string to 'AAAAAA' initially. */
+            for (i = 0; i < 7; i++)
+            {
+                result[i] = 'A';
+            }
+
+            /* Convert unique values from right to left into an alphabetic string in tmp */
+            /* temp digits are right-justified in tmp */
+            i = 6;
+            count = 0;
+            while (unique > 0)
+            {
+                remainder = unique % 26;
+                tmp[i] = (char)('A' + remainder);
+                unique /= 26;
+                i--;
+                count++;
+            }
+
+            /* Finally, move tmp into result, left justifying it. */
+            for (j = 0; j < count; j++, i++)
+            {
+                result[j] = tmp[6-j];
+            }
+            return new string(result);
+        }
+
+        public static string SpaceConvert(long unique)
+        {
+            char[] tmp = new char[7];
+            char[] result = new char[7];
+            long i, j, remainder, count;
+
+            /* Set the result string to 'AAAAAA' initially. */
+            for (i = 0; i < 7; i++)
+            {
+                result[i] = ' ';
+            }
+
+            /* Convert unique values from right to left into an alphabetic string in tmp */
+            /* temp digits are right-justified in tmp */
+            i = 6;
+            count = 0;
+            while (unique > 0)
+            {
+                remainder = unique % 26;
+                tmp[i] = (char)('A' + remainder);
+                unique /= 26;
+                i--;
+                count++;
+            }
+
+            /* We went one index too far with i, so we need to correct. */
+            i++;
+
+            /* Finally, move tmp into result, left justifying it. */
+            for (j = 0; j < count; j++, i++)
+            {
+                result[j] = tmp[i];
+            }
+            return new string(result);
+        }
+
+        public static string OrderedConvert(long unique)
+        {
+            char[] tmp = new char[7];
+            char[] result = new char[7];
+            long i, j, remainder;
+
+            /* Set the result string to 'AAAAAA' initially. */
+            for (i = 0; i < 7; i++)
+            {
+                result[i] = 'A';
+            }
+
+            /* Convert unique values from right to left into an alphabetic string in tmp */
+            /* temp digits are right-justified in tmp */
+            i = 6;
+            while (unique > 0)
+            {
+                remainder = unique % 26;
+                tmp[i] = (char)('A' + remainder);
+                unique /= 26;
+                i--;
+            }
+
+            /* Finally, move tmp into result, left justifying it. */
+            for (i = i + 1; i <= 6; i++)
+            {
+                result[i] = tmp[i];
+            }
+            return new string(result);
+        }
+    }
+}
