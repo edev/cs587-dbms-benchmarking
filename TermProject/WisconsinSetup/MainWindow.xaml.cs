ad2f91f6527e8e922429a08444dcc34b56412330
Dylan Laufenberg <edev455@gmail.com>
2019-04-19 00:22:45 -0700

    Checkpoint: Migrate most logic from MainWindow code-behind to its associated ModelView and create relevant bindings. Untested and doesn't yet compile.

--- TermProject/WisconsinSetup/MainWindow.xaml.cs
+++ TermProject/WisconsinSetup/MainWindow.xaml.cs
@@ -26,10 +26,6 @@ namespace WisconsinSetup
     {
         private readonly MainWindowViewModel _viewModel;
 
-        private SQC.SqlConnection Connection { get; set; } = new SQC.SqlConnection();
-
-        private TableManager _tableManager = null;
-        
         private Multipliers MultiplierCollection = new Multipliers();
 
         public MainWindow()
@@ -37,7 +33,7 @@ namespace WisconsinSetup
             InitializeComponent();
             _viewModel = new MainWindowViewModel();
             DataContext = _viewModel;
-            TbLog.TextChanged += LogTextChanged;
+            TbLog.TextChanged += LogTextChanged; // Required so we can scroll to end when log entries come in.
         }
 
         /* Scroll to the end of the log any time it changes. This event is fired when
@@ -48,100 +44,26 @@ namespace WisconsinSetup
             TbLog.ScrollToEnd();
         }
 
-        /* This function contains the exact instructions for making a given table,
-         * expressed in terms of TableManager methods.
-         TODO Make this async, or run in a background task, or whatever. Don't lock up the UI thread.
-         */
-        private void _makeTable(string tableName, long tableSize)
-        {
-            StringBuilder resultString = new StringBuilder();
-            if (_tableManager == null)
-            {
-                _viewModel.LogEntry("Table manager is not ready.");
-                return;
-            }
-
-            var watch = new Stopwatch();
-            watch.Start();
-            resultString.AppendLine($"Make table: {tableName}");
-            resultString.AppendLine(_tableManager.DropTableIfExists(tableName));
-            resultString.AppendLine(_tableManager.CreateTable(tableName));
-            var relation = new Relation(tableName, tableSize);
-            resultString.AppendLine($"Write CSV");
-            relation.WriteCsv();
-            // resultString.AppendLine(_tableManager.InsertRows(relation));
-            resultString.AppendLine(_tableManager.BulkInsert(tableName, relation.CsvFilename));
-
-            resultString.AppendLine("Done.");
-            watch.Stop();
-            // Copied from official example: https://docs.microsoft.com/en-us/dotnet/api/system.diagnostics.stopwatch?redirectedfrom=MSDN&view=netframework-4.8
-            // Get the elapsed time as a TimeSpan value.
-            TimeSpan ts = watch.Elapsed;
-
-            // Format and display the TimeSpan value.
-            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
-                ts.Hours, ts.Minutes, ts.Seconds,
-                ts.Milliseconds / 10);
-            resultString.AppendLine($"Elapsed time: {elapsedTime}");
-            _viewModel.LogEntry(resultString.ToString());
-        }
-
-        private void _dropTable(string tableName)
-        {
-            if (_tableManager == null)
-            {
-                _viewModel.LogEntry("Table manager is not ready.");
-                return;
-            }
-            _viewModel.LogEntry($"Drop table: {tableName}");
-            _viewModel.LogEntry(_tableManager.DropTableIfExists(tableName));
-        }
-
         private long Table1Size => Convert.ToInt64(TbRows1.Text) * (Multipliers.Mappings[(string)CbMultiplier1.SelectedValue]);
         private long Table2Size => Convert.ToInt64(TbRows3.Text) * (Multipliers.Mappings[(string)CbMultiplier2.SelectedValue]);
         private long Table3Size => Convert.ToInt64(TbRows3.Text) * (Multipliers.Mappings[(string)CbMultiplier3.SelectedValue]);
 
+        /* Ask the ViewModel to connect using the user's connection string. */
         private void BtnConnect_OnClick(object sender, RoutedEventArgs e)
         {
-            try
-            {
-                _tableManager = null;
-                if (Connection.State == ConnectionState.Open)
-                {
-                    _viewModel.LogEntry("Closing connection.");
-                    Connection.Close();
-                }
-
-                // Bindings were NOT working properly, so let's just use the tried-and-true method
-                // of pulling from and pushing to the form by hand. Simple, easy, and correct.
-                Connection.ConnectionString = TbConnectionString.Text;
-                Connection.Open();
-                _tableManager = new TableManager(Connection);
-                _viewModel.LogEntry("Connection open.");
-            }
-            catch (Exception exc)
-            {
-                _viewModel.LogEntry(exc.Message);
-            }
-            LblConnectionStatus.Content = Connection.State;
+            _viewModel.Connect();
         }
 
         private void BtnDisconnect_OnClick(object sender, RoutedEventArgs e)
         {
-            if (Connection.State == ConnectionState.Open)
-            {
-                _viewModel.LogEntry("Closing connection.");
-            }
-            Connection.Close();
-            LblConnectionStatus.Content = Connection.State;
-            _tableManager = null;
+            _viewModel.Disconnect();
         }
 
         private void BtnCreateTable1_OnClick(object sender, RoutedEventArgs e)
         {
             try
             {
-                _makeTable(TbTableName1.Text, Table1Size);
+                _viewModel.MakeTable(TbTableName1.Text, Table1Size);
             }
             catch (System.FormatException)
             {
@@ -153,7 +75,7 @@ namespace WisconsinSetup
         {
             try
             {
-                _makeTable(TbTableName2.Text, Table2Size);
+                _viewModel.MakeTable(TbTableName2.Text, Table2Size);
             }
             catch (System.FormatException)
             {
@@ -165,7 +87,7 @@ namespace WisconsinSetup
         {
             try
             {
-                _makeTable(TbTableName3.Text, Table3Size);
+                _viewModel.MakeTable(TbTableName3.Text, Table3Size);
             }
             catch (System.FormatException)
             {
@@ -177,9 +99,9 @@ namespace WisconsinSetup
         {
             try
             {
-                _makeTable(TbTableName1.Text, Table1Size);
-                _makeTable(TbTableName2.Text, Table2Size);
-                _makeTable(TbTableName3.Text, Table3Size);
+                _viewModel.MakeTable(TbTableName1.Text, Table1Size);
+                _viewModel.MakeTable(TbTableName2.Text, Table2Size);
+                _viewModel.MakeTable(TbTableName3.Text, Table3Size);
             }
             catch (System.FormatException)
             {
@@ -189,24 +111,24 @@ namespace WisconsinSetup
          
         private void BtnDropTable1_OnClick(object sender, RoutedEventArgs e)
         {
-            _dropTable(TbTableName1.Text);
+            _viewModel.DropTable(TbTableName1.Text);
         }
 
         private void BtnDropTable2_OnClick(object sender, RoutedEventArgs e)
         {
-            _dropTable(TbTableName2.Text);
+            _viewModel.DropTable(TbTableName2.Text);
         }
 
         private void BtnDropTable3_OnClick(object sender, RoutedEventArgs e)
         {
-            _dropTable(TbTableName3.Text);
+            _viewModel.DropTable(TbTableName3.Text);
         }
 
         private void BtnDropAll_OnClick(object sender, RoutedEventArgs e)
         {
-            _dropTable(TbTableName1.Text);
-            _dropTable(TbTableName2.Text);
-            _dropTable(TbTableName3.Text);
+            _viewModel.DropTable(TbTableName1.Text);
+            _viewModel.DropTable(TbTableName2.Text);
+            _viewModel.DropTable(TbTableName3.Text);
         }
     }
 }
--- TermProject/WisconsinSetup/MainWindowViewModel.cs
+++ TermProject/WisconsinSetup/MainWindowViewModel.cs
@@ -1,15 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using SQC = System.Data.SqlClient;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Diagnostics;
 
 namespace WisconsinSetup
 {
     class MainWindowViewModel : INotifyPropertyChanged
     {
+        public readonly SQC.SqlConnection Connection = new SQC.SqlConnection();
+
+        private TableManager _tableManager = null;
+
+        public MainWindowViewModel()
+        {
+            // Set the initially selected multipliers to the first one in the list.
+            // We can't do this using initialization syntax, which is not shocking.
+            Multiplier1 = Multipliers[0];
+            Multiplier2 = Multipliers[0];
+            Multiplier3 = Multipliers[0];
+        }
+
         // ========
         // SECTION: INotifyPropertyChanged
         // ========
@@ -54,5 +71,192 @@ namespace WisconsinSetup
             NotifyPropertyChanged("Log");
         }
 
+        // ========
+        // SECTION: Connect/disconnect
+        // ========
+
+        // Connection state, with notifier.
+        private ConnectionState _connectionState = ConnectionState.Closed;
+        public ConnectionState ConnectionState
+        {
+            get => _connectionState;
+            set
+            {
+                _connectionState = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        // Connection string, with notifier.
+        public string ConnectionString
+        {
+            get => Connection.ConnectionString;
+            set
+            {
+                Connection.ConnectionString = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        // Attempt to connect to the database using the connection string that the user
+        // specified in the UI (which was hopefully bound via ConnectionString).
+        public void Connect()
+        {
+            try
+            {
+                // For safety and DRY-ness, we'll close and clear any previous resources.
+                // If we our connection attempt is successful, we'll open up again.
+                ConnectionState = ConnectionState.Closed;
+                _tableManager = null;
+                if (Connection.State == ConnectionState.Open)
+                {
+                    LogEntry("Closing connection.");
+                    Connection.Close();
+                }
+
+                Connection.Open();
+
+                // If we reach this point without raising an error, we have an open connection,
+                // and all is well. Time to open up shop!
+                _tableManager = new TableManager(Connection);
+                ConnectionState = Connection.State;
+                LogEntry("Connection open.");
+            }
+            catch (Exception exc)
+            {
+                // Connection.Open() threw an error, most likely.
+                // Log it and move on.
+                LogEntry(exc.Message);
+            }
+        }
+
+        public void Disconnect()
+        {
+            if (Connection.State == ConnectionState.Open)
+            {
+                LogEntry("Closing connection.");
+            }
+
+            Connection.Close();
+            ConnectionState = Connection.State;
+            _tableManager = null;
+        }
+
+        // ========
+        // SECTION: Table Sizes
+        // ========
+
+        
+
+        // ========
+        // SECTION: Table Size Multipliers
+        // ========
+
+        // Our collection of multipliers, which populates the selectable list in the UI.
+        public ObservableCollection<Multiplier> Multipliers = new ObservableCollection<Multiplier>
+        {
+            new Multiplier("1x", 1),
+            new Multiplier("Thousand", 1000),
+            new Multiplier("Million", 1000000)
+        };
+
+        // NOTE: Multipliers for the three tables are initialized in the constructor.
+
+        // The currently selected multiplier for table 1.
+        private Multiplier _multiplier1;
+        public Multiplier Multiplier1
+        {
+            get => _multiplier1;
+            set
+            {
+                _multiplier1 = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        // The currently selected multiplier for table 2.
+        private Multiplier _multiplier2;
+        public Multiplier Multiplier2
+        {
+            get => _multiplier2;
+            set
+            {
+                _multiplier2 = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        // The currently selected multiplier for table 3.
+        private Multiplier _multiplier3;
+        public Multiplier Multiplier3
+        {
+            get => _multiplier3;
+            set
+            {
+                _multiplier3 = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        // ========
+        // SECTION: Drop Table
+        // ========
+
+        public void DropTable(string tableName)
+        {
+            if (_tableManager == null)
+            {
+                LogEntry("Table manager is not ready.");
+                return;
+            }
+            LogEntry($"Drop table: {tableName}");
+            LogEntry(_tableManager.DropTableIfExists(tableName), 1); // TODO Clean up the indentation mess elsewhere!
+        }
+
+        // ========
+        // SECTION: Make Table
+        // ========
+
+        /* This function contains the exact instructions for making a given table,
+         * expressed in terms of TableManager methods.
+         TODO Make this async, or run in a background task, or whatever. Don't lock up the UI thread.
+         */
+        public void MakeTable(string tableName, long tableSize)
+        {
+            if (_tableManager == null)
+            {
+                LogEntry("Table manager is not ready.");
+                return;
+            }
+
+            // We'll time the whole operation so we can report elapsed time at the end.
+            var watch = new Stopwatch();
+            watch.Start();
+
+            // First, we'll drop and re-create the table.
+            LogEntry($"Make table: {tableName}");
+            LogEntry(_tableManager.DropTableIfExists(tableName), 1);
+            LogEntry(_tableManager.CreateTable(tableName), 1);
+
+            // Next, we'll generate our rows and write our CSV file.
+            var relation = new Relation(tableName, tableSize);
+            LogEntry($"Write CSV"); // TODO Save some time by incorporating a row count into CSV filename and reusing it if it exists!
+            relation.WriteCsv();
+
+            // Finally, we'll issue a bulk load command to our DBMS.
+            LogEntry(_tableManager.BulkInsert(tableName, relation.CsvFilename));
+
+            // Done!
+            LogEntry("Done.");
+            watch.Stop();
+
+            // Copied from official Microsoft example:
+            // https://docs.microsoft.com/en-us/dotnet/api/system.diagnostics.stopwatch?redirectedfrom=MSDN&view=netframework-4.8
+            TimeSpan ts = watch.Elapsed;  // Get the elapsed time as a TimeSpan value.
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            LogEntry($"Elapsed time: {elapsedTime}");
+        }
     }
 }
